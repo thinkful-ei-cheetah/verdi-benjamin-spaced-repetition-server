60975b98954e2f34e2f4d41875cdb7a950324e13
Ben Tilghman <ben.tilghman@gmail.com>
2019-06-24 10:49:03 -0400

    update seeds

--- seeds/seed.tables.sql
+++ seeds/seed.tables.sql
@@ -21,14 +21,15 @@ VALUES
 
 INSERT INTO "word" ("id", "language_id", "original", "translation", "next")
 VALUES
-  (1, 1, 'entraine toi', 'practice', 2),
-  (2, 1, 'bonjour', 'hello', 3),
-  (3, 1, 'maison', 'house', 4),
-  (4, 1, 'développeur', 'developer', 5),
-  (5, 1, 'traduire', 'translate', 6),
-  (6, 1, 'incroyable', 'amazing', 7),
-  (7, 1, 'chien', 'dog', 8),
-  (8, 1, 'chat', 'cat', null);
+  (1, 1, 'un', 'one', 2),
+  (2, 1, 'deux', 'two', 3),
+  (3, 1, 'trois', 'three', 4),
+  (4, 1, 'quatre', 'four', 5),
+  (5, 1, 'cinq', 'five', 6),
+  (6, 1, 'six', 'six', 7),
+  (7, 1, 'sept', 'seven', 8),
+  (8, 1, 'huit', 'eight', null);
+
 
 UPDATE "language" SET head = 1 WHERE id = 1;
 
